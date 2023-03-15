2639237bfc4bc3932b9fe752978af632c55e17b8
CalltheCodeDoc <98482948+CalltheCodeDoc@users.noreply.github.com>
2023-03-14 21:39:40 -0400

    OK changed the upkeep system to dramatically simplify code, made it way bettter, removed annoying errors caused by upkeep system

--- Source.cpp
+++ Source.cpp
@@ -14,9 +14,13 @@
 
 
 using namespace std;
+
+//prototypes
+InventoryItem* searchsku2(int SKU, LinkedList<InventoryItem>* lis);
 InventoryItem* searchsku(int SKU, InventoryItem* arr[], int numItems);
 int indexsku(int SKU, InventoryItem* arr[], int numitems);
 void arraysquish(InventoryItem* arr[], int index, int numitems);
+void displayskus(LinkedList<InventoryItem>* lis);
 
 int main() {
 	//commented code tests parts of the code as I go
@@ -123,25 +127,43 @@ int main() {
 	Node<InventoryItem>* temp = new Node<InventoryItem>(new InventoryItem(576879, "PS5 gaming console", 600, "UOM whatever that is", 5, 20), list->head);
 	//^^^^ Don't do node<inventoryItem
 
+
+
+	list->AddItem(new InventoryItem(3879, "PS5 gaming console", 600, "UOM whatever that is", 5, 20));
+		list->AddItem(new InventoryItem(2589, "PS5 gaming console", 600, "UOM whatever that is", 5, 20));
+		list->AddItem(new InventoryItem(1687, "PS5 gaming console", 600, "UOM whatever that is", 5, 20));
+		list->AddItem(new InventoryItem(9579, "PS5 gaming console", 600, "UOM whatever that is", 5, 20));
+		list->AddItem(new InventoryItem(5879, "PS5 gaming console", 600, "UOM whatever that is", 5, 20));
+			list->AddItem(new InventoryItem(589, "PS5 gaming console", 600, "UOM whatever that is", 5, 20));
+				list->AddItem(new InventoryItem(5687, "PS5 gaming console", 600, "UOM whatever that is", 5, 20));
+					list->AddItem(new InventoryItem(4579, "PS5 gaming console", 600, "UOM whatever that is", 5, 20));
+		
+		/*
 	InventoryItem* arrg[30] = { new InventoryItem(5879, "PS5 gaming console", 600, "UOM whatever that is", 5, 20)
 	,new InventoryItem(589, "PS5 gaming console", 600, "UOM whatever that is", 5, 20)
 	,new InventoryItem(5687, "PS5 gaming console", 600, "UOM whatever that is", 5, 20)
 	,new InventoryItem(4579, "PS5 gaming console", 600, "UOM whatever that is", 5, 20) };
+	//we only have to manage the array if when we delete these pointers within the linked list
+	// the items dont get deleted
+	//if they do get deleted then the array is only then necessary to access all the SKU's
+	//easily, but there are non-obtrusive ways to access the SKU's, through search the list
+	// directly instead of storing them in an array that requires so much management
+	//so fragile too.
 
 	for (int i = 0; i < 4; i++) {
 
 		list->AddItem(arrg[i]);
 	}
-
+	*/
 	list->NotYetMergeSort();
 	list->Display();
 
 	Node<InventoryItem>* temp3;
-	temp3 = list->GetItem(searchsku(5879, arrg, numberitems), list->head); //uh oh u see the problem gEttIng an inventory item is only done by pointer
+	//temp3 = list->GetItem(searchsku(5879, arrg, numberitems), list->head); //uh oh u see the problem gEttIng an inventory item is only done by pointer
 	// but unless u create a inventory pointer to descriptoin table
 	//you wont be able to search it
 	// sollution is make something that can search by sku or description
-	cout << temp3->data->GetPartInfo() << endl;
+	//cout << temp3->data->GetPartInfo() << endl;
 
 
 
@@ -174,13 +196,14 @@ int main() {
 			cout << "Type 11 for: void Reset();" << endl;
 			cout << "Type 12 for: void NotYetMergeSort();" << endl;
 			cout << "Type 13 for: void Display();" << endl;
+			//cout << "Type 14 for: void displayskus(LinkedList<InventoryItem>* lis)" << endl;
 			cout << "Type 99 to terminate program" << endl;
 			cin >> userresponse;
 			cin.ignore();
 			//getline(cin, userresponse);
 
 	} while (userresponse>13||userresponse <1);
-
+	cin.clear();
 	switch (userresponse) {
 	case 1:
 		//cout << "Creating empty list..." << endl;
@@ -215,11 +238,12 @@ int main() {
 		cin.ignore();
 		cout << "Description: " << endl;
 		getline(cin, desc);
-		arrg[numberitems] = new InventoryItem(SKU, desc, price, uom, lead, quantity);
 		cout << "Adding item to list..." << endl;
-		list->AddItem(arrg[numberitems]);
-		numberitems++;
+		list->AddItem(new InventoryItem(SKU, desc, price, uom, lead, quantity));
+		list->NotYetMergeSort();
+		//numberitems++;
 		break;
+		/*
 	case 4:
 		cout << "Type in the SKU of the item you want to get: " << endl;
 		//hmmmmmmmmmmm  T* item = InventoryItem, but how to get the SKU?
@@ -245,12 +269,34 @@ int main() {
 		numberitems--;
 		break;
 		//add additional code to display the item to the user indicating its in stock
+		*/
+	case 4:
+		cout << "Type in the SKU of the item you want to get: " << endl;
+		cin >> SKU;
+		cin.ignore();
+		//if not available, message this to customer,
+		//or just use available() method
+		//arrgindex = indexsku(SKU, arrg, numberitems);
+		//Node<InventoryItem>* searchsku(int SKU, LinkedList<InventoryItem>*lis)
+		temp3 = list->GetItem(searchsku2(SKU, list), list->head);
+		cout << temp3->data->GetPartInfo() << " Is the item retrieved " << endl;
+		delete temp3->data;
+		temp3->data = nullptr;
+		//probably should make destructor handle this stuff automatically
+		delete temp3;
+		temp3 = nullptr;
+		//delete arrg[arrgindex];
+		//arrg[arrgindex] = nullptr;
+		//arraysquish(arrg, arrgindex, numberitems);
+		//numberitems--;
+		break;
+		//add additional code to display the item to the user indicating its in stock
 	case 5:
 		cout << "Check to see if an item is in the list." << endl;
 		cout << "Type in the SKU of the item you want to check: " << endl;
 		cin >> SKU;
 		cin.ignore();
-		Isin = list->IsInList(searchsku(SKU, arrg, numberitems)); //replace T * item with proper coding
+		Isin = list->IsInList(searchsku2(SKU, list)); //replace T * item with proper coding
 		if (Isin) {
 			cout << "Item is in the list." << endl;
 		}
@@ -281,7 +327,7 @@ int main() {
 		cout << "Type in the SKU of the item you want to see: "<<endl;
 		cin >> SKU;
 		//might have to change function to pay attention to SKU, not pointer
-		temp = list->SeeAt(searchsku(SKU, arrg, numberitems), list->head);
+		temp = list->SeeAt(searchsku2(SKU, list), list->head);
 		cout << temp->data <<endl; //this sees a pointer, probably wanna see SKU or description
 		//also replace T* with some mangled mess of data->getSKU(SKU)
 		//*************************************************************
@@ -303,13 +349,17 @@ int main() {
 		cout << "Display the list." <<endl;
 		list->Display();
 		break;
+	//case 14:
+	//	cout << "Display all the SKUS in the list." << endl;
+	//	displayskus(list);
+	//	break;
 	case 99:
 		cout << "Terminating program." <<endl;
 	}
 	cout<< "Clear text? (y/n)" <<endl;
 	cin>>userresponse1;
 	if(userresponse1=='y')
-	system("clr");
+	system("CLS");
 		}while (userresponse!=99);
 
 	
@@ -323,7 +373,19 @@ int main() {
 
 
 
+InventoryItem* searchsku2(int SKU, LinkedList<InventoryItem>* lis) {
+	Node<InventoryItem>* tempo = lis->head;
+	int suk;
+	while (tempo!=nullptr) {
+		suk = tempo->data->GetSKU();
+		if (suk == SKU) {
+			return tempo->data;
+		}
+		tempo=tempo->next;
+	}
+	return nullptr;
 
+}
 
 InventoryItem* searchsku(int SKU, InventoryItem* arr[], int numitems) {
 	for (int i = 0; i < numitems; i++) {
@@ -352,4 +414,15 @@ void arraysquish(InventoryItem* arr[], int index, int numitems) {
 			arr[k] = arr[k + 1];
 		}
 	}
+}
+
+void displayskus(LinkedList<InventoryItem>* lis) {
+	int sku;
+	for (int i = 0; i < lis->Size(); i++) {
+		sku = lis->SeeNext()->GetSKU();
+		cout<< sku <<endl;
+		
+
+	}
+	lis->Reset();
 }
